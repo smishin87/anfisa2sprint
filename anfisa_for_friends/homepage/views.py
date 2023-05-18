385435800c1850332074fc42e781a27ca113f419
Sergei Mishin <SMishinMail@gmail.com>
2023-05-18 18:03:32 +0700

    Adds data from models to templates

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from ice_cream.models import IceCream
+from django.db.models import Q
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values('id', 'title')\
+        .filter(Q(is_published=True) & (Q(is_on_main=True) | Q(title__contains='пломбир')))
+    context = {'ice_cream_list': ice_cream_list}
+    return render(request, template, context)
